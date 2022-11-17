d317d33bd0bd09979760ddfd24d7fafb8c69fa8e
Daniel Courtney <dan4310@gmail.com>
2022-11-16 20:20:36 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# danielmcourtney.com
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import '$styles/index.scss'
+</script>
+
+<svelte:head>
+    <title>Daniel Courtney</title>
+</svelte:head>
+
+<slot />
