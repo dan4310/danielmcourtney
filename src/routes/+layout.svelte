080a2b15167a558d9c579f5b804159b1b07d0260
Daniel Courtney <dan4310@gmail.com>
2022-11-16 21:43:53 -0500

    feat(navbar): basic navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,4 +6,16 @@
 	<title>Daniel Courtney</title>
 </svelte:head>
 
+
+<header class="navbar">
+	<div class="">
+		<img src="images/michaelangelo.png" alt="Me icon" />
+	</div>
+	<ul class="navbar__links">
+		<li><a class="navbar__link" href="/about">About</a></li>
+		<li><a class="navbar__link" href="/experience">Experience</a></li>
+		<li><a class="navbar__link" href="/projects">Projects</a></li>
+	</ul>
+</header>
 <slot />
+
