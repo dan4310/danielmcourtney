a2d82b0a9894788209cb14f61007203a8b02b176
Daniel Courtney <dan4310@gmail.com>
2022-11-19 16:08:44 -0500

    feat(drawer): headlessui and drawer

---
+++ src/lib/components/Drawer.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+	import { DialogOverlay, Dialog, DialogTitle } from '@rgossiaux/svelte-headlessui'
+	import { createEventDispatcher } from 'svelte'
+	import { fly, fade } from 'svelte/transition'
+
+	export let open = false
+	const dispatch = createEventDispatcher()
+</script>
+
+<Dialog {open} on:close={() => dispatch('close')}>
+	<div
+		class="container"
+		transition:fade={{
+			duration: 150
+		}}
+	>
+		<DialogOverlay class="overlay" />
+
+		<aside
+			class="drawer"
+			transition:fly={{
+				x: 325,
+				duration: 150
+			}}
+		>
+			<DialogTitle>
+				<div class="drawer__header">
+					<button type="button" class="btn-outline-primary" on:click={() => dispatch('close')}
+						>X</button
+					>
+				</div>
+			</DialogTitle>
+			<div class="drawer__content">
+				<slot />
+			</div>
+		</aside>
+	</div>
+</Dialog>
+
+<style lang="scss">
+	@use '../../styles/variables' as var;
+
+	.container {
+		position: fixed;
+		top: 0;
+		display: flex;
+		width: 100%;
+		height: 100%;
+		background-color: rgba(black, 0.5);
+	}
+
+	.drawer {
+		width: 100%;
+		max-width: 325px;
+		flex: 2;
+		background-color: var.$theme-background;
+		.drawer__header {
+			display: flex;
+			justify-content: end;
+		}
+	}
+
+	:global(.overlay) {
+		flex: 1;
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
+	import Drawer from '$lib/components/Drawer.svelte'
 	import '$styles/index.scss'
 	let scrollY = 0
+
+	let open = false
 </script>
 
 <svelte:window bind:scrollY />
@@ -18,15 +21,48 @@
 			<li><a class="navbar__link" href="/about">About</a></li>
 			<li><a class="navbar__link" href="/experience">Experience</a></li>
 			<li><a class="navbar__link" href="/projects">Projects</a></li>
+			<li><a class="btn-outline-primary" href="/contact">Contact</a></li>
 		</ul>
-		<a class="btn-outline-primary" href="/contact">Contact</a>
+		<button class="btn-menu btn-primary" type="button" on:click={() => (open = true)}>M</button>
+		<Drawer {open} on:close={() => (open = false)}>
+			<ul class="sidemenu">
+				<li><a class="sidemenu__link" href="/about">About</a></li>
+				<li><a class="sidemenu__link" href="/experience">Experience</a></li>
+				<li><a class="sidemenu__link" href="/projects">Projects</a></li>
+			</ul>
+		</Drawer>
 	</div>
 </header>
 <slot />
 
-<style>
+<style lang="scss">
+	@use '../styles/breakpoints' as b;
+	@use '../styles/variables' as var;
+
 	.avatar {
 		width: 40px;
 		border-radius: 100px;
+	}
+	.btn-menu {
+		@include b.break(sm) {
+			display: none;
+		}
+	}
+
+	.sidemenu {
+		display: flex;
+		flex-direction: column;
+		gap: var.$padding;
+		li {
+			display: flex;
+			&:hover {
+				color: var.$theme-text-muted;
+			}
+		}
+		.sidemenu__link {
+			width: 100%;
+			display: flex;
+			justify-content: center;
+		}
 	}
 </style>
